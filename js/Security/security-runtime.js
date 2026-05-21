64b54dff1727b9bdc01dc47bd6a423dccbe3f3d6
Rigo-com <azizo436618@gmail.com>
2026-05-21 21:35:13 +0300

    Update security-runtime.js

--- js/Security/security-runtime.js
+++ js/Security/security-runtime.js
@@ -1,1 +1,77 @@
+function registerSecurityPatterns(){
 
+  Object.values(
+    SECURITY_PATTERNS
+  )
+  .flat()
+  .forEach((pattern) => {
+
+    securityState
+    .blockedPatterns
+    .add(
+      pattern
+    );
+
+  });
+
+  return true;
+
+}
+
+
+
+function logSecurityEvent(
+  message,
+  metadata = null
+){
+
+  if(
+
+    !SECURITY_CONFIG
+    .ENABLE_SECURITY_LOGGING
+
+  ){
+
+    return false;
+
+  }
+
+  try{
+
+    if(
+      typeof logDiagnosticWarning ===
+      "function"
+    ){
+
+      logDiagnosticWarning(
+
+        "[SECURITY] " +
+        String(message),
+
+        metadata
+
+      );
+
+    }
+
+    else{
+
+      console.warn(
+        "[SECURITY]",
+        message,
+        metadata || ""
+      );
+
+    }
+
+  }
+
+  catch(error){
+
+    return false;
+
+  }
+
+  return true;
+
+}
