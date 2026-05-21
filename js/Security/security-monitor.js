002e66f20329e52d14cdf6299fcf0732423df89e
Rigo-com <azizo436618@gmail.com>
2026-05-21 21:37:07 +0300

    Update security-monitor.js

--- js/Security/security-monitor.js
+++ js/Security/security-monitor.js
@@ -1,1 +1,33 @@
+function cleanupRateLimitTracker(){
 
+  const now =
+  Date.now();
+
+  while(
+
+    securityState
+    .requestTracker
+    .size >
+
+    SECURITY_CONFIG
+    .MAX_TRACKED_KEYS
+
+  ){
+
+    const firstKey =
+
+      securityState
+      .requestTracker
+      .keys()
+      .next()
+      .value;
+
+    securityState
+    .requestTracker
+    .delete(
+      firstKey
+    );
+
+  }
+
+}
