64ce0b4145276793a1fc3d9ef90584337f3cb5eb
Rigo-com <azizo436618@gmail.com>
2026-05-21 21:38:09 +0300

    Update security-report.js

--- js/Security/security-report.js
+++ js/Security/security-report.js
@@ -1,1 +1,35 @@
+function generateSecurityReport(){
 
+  return deepFreezeSecurity({
+
+    initialized:
+    securityState
+    .initialized,
+
+    blockedRequests:
+    securityState
+    .blockedRequests,
+
+    suspiciousActivities:
+    securityState
+    .suspiciousActivities,
+
+    sanitizedPayloads:
+    securityState
+    .sanitizedPayloads,
+
+    blockedURLs:
+    securityState
+    .blockedURLs,
+
+    blockedPrompts:
+    securityState
+    .blockedPrompts,
+
+    rateLimitHits:
+    securityState
+    .rateLimitHits
+
+  });
+
+}
