586ff48ceb2b6505d1db1ece11800a2db425f87c
Rigo-com <azizo436618@gmail.com>
2026-05-21 21:38:44 +0300

    Update security-sandbox.js

--- js/Security/security-sandbox.js
+++ js/Security/security-sandbox.js
@@ -1,1 +1,30 @@
+function executeInSandbox(
+  callback
+){
 
+  if(
+    typeof callback !==
+    "function"
+  ){
+
+    return null;
+
+  }
+
+  try{
+
+    return callback();
+
+  }
+
+  catch(error){
+
+    logSecurityEvent(
+      "SANDBOX EXECUTION FAILED"
+    );
+
+    return null;
+
+  }
+
+}
