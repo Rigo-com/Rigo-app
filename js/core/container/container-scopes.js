f37574b353dc02a57cf030300fa97f2ec64e375f
Rigo-com <azizo436618@gmail.com>
2026-05-22 23:21:01 +0300

    Update container-scopes.js

--- js/core/container/container-scopes.js
+++ js/core/container/container-scopes.js
@@ -1,1 +1,94 @@
+// =====================================
+// RIGO AI
+// CONTAINER SCOPES
+// =====================================
 
+
+
+// =====================================
+// CIRCULAR CHECK
+// =====================================
+
+function detectCircularDependency(
+  serviceName
+){
+
+  if(
+
+    !DEPENDENCY_CONTAINER_CONFIG
+    .ENABLE_CIRCULAR_PROTECTION
+
+  ){
+
+    return false;
+
+  }
+
+  return (
+
+    dependencyContainerState
+    .resolutionStack
+    .includes(
+      serviceName
+    )
+
+  );
+
+}
+
+
+
+// =====================================
+// SCOPES
+// =====================================
+
+function getScopeContainer(
+  scope
+){
+
+  const normalizedScope =
+  normalizeServiceName(
+    scope
+  );
+
+  if(
+    !normalizedScope
+  ){
+
+    return null;
+
+  }
+
+  if(
+
+    !dependencyContainerState
+    .scopes
+    .has(
+      normalizedScope
+    )
+
+  ){
+
+    dependencyContainerState
+    .scopes
+    .set(
+
+      normalizedScope,
+
+      new Map()
+
+    );
+
+    dependencyContainerState
+    .diagnostics
+    .scopes++;
+
+  }
+
+  return dependencyContainerState
+  .scopes
+  .get(
+    normalizedScope
+  );
+
+}
