972c383c21418d22a13a52199c2eb3f97328dc66
Rigo-com <azizo436618@gmail.com>
2026-05-23 01:07:20 +0300

    Create health-system.js

---
+++ js/core/health/health-system.js
@@ -0,0 +1,27 @@
+// =====================================
+// RIGO AI
+// HEALTH SYSTEM
+// =====================================
+
+
+
+// =====================================
+// PUBLIC API
+// =====================================
+
+const HealthSystem =
+Object.freeze({
+
+  run:
+  runAppHealthcheck,
+
+  start:
+  startHealthchecks,
+
+  stop:
+  stopHealthchecks,
+
+  diagnostics:
+  getHealthDiagnostics
+
+});
