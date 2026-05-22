d92d29c18f8cb70e5f496134e2454a375f2acf4a
Rigo-com <azizo436618@gmail.com>
2026-05-22 22:35:06 +0300

    Create system-event-priorities.js

---
+++ js/core/constants/system-event-priorities.js
@@ -0,0 +1,17 @@
+// =====================================
+// RIGO AI
+// SYSTEM EVENT PRIORITIES
+// =====================================
+
+const SYSTEM_EVENT_PRIORITIES =
+Object.freeze({
+
+  LOW:1,
+
+  NORMAL:5,
+
+  HIGH:10,
+
+  CRITICAL:20
+
+});
