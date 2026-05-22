f1b416a14a54ac019fc72e0f9dee9a05d98f2c76
Rigo-com <azizo436618@gmail.com>
2026-05-22 22:33:24 +0300

    Create system-event-config.js

---
+++ js/core/constants/system-event-config.js
@@ -0,0 +1,48 @@
+// =====================================
+// RIGO AI
+// SYSTEM EVENT CONFIG
+// =====================================
+
+const SYSTEM_EVENTS_CONFIG =
+Object.freeze({
+
+  ENABLE_HISTORY:true,
+
+  ENABLE_WILDCARDS:true,
+
+  ENABLE_PRIORITIES:true,
+
+  ENABLE_REPLAY:true,
+
+  ENABLE_MIDDLEWARE:true,
+
+  ENABLE_THROTTLING:true,
+
+  ENABLE_DIAGNOSTICS:true,
+
+  ENABLE_QUEUE:true,
+
+  ENABLE_RETRIES:true,
+
+  MAX_EVENT_HISTORY:
+  5000,
+
+  MAX_LISTENERS:
+  500,
+
+  MAX_EVENT_QUEUE:
+  1000,
+
+  EVENT_TIMEOUT:
+  15000,
+
+  MAX_RETRIES:
+  3,
+
+  THROTTLE_CLEANUP_INTERVAL:
+  60000,
+
+  MAX_THROTTLED_EVENTS:
+  5000
+
+});
