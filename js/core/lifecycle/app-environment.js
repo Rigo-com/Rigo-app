0403387ecac5095cfc6fabb1d27ff036abc128ed
Rigo-com <azizo436618@gmail.com>
2026-05-22 23:35:46 +0300

    Update app-environment.js

--- js/core/lifecycle/app-environment.js
+++ js/core/lifecycle/app-environment.js
@@ -1,1 +1,34 @@
+// =====================================
+// RIGO AI
+// APP ENVIRONMENT
+// =====================================
 
+
+
+// =====================================
+// VALIDATE APP
+// =====================================
+
+function validateAppEnvironment(){
+
+  if(
+    typeof window ===
+    "undefined"
+  ){
+
+    return false;
+
+  }
+
+  if(
+    typeof document ===
+    "undefined"
+  ){
+
+    return false;
+
+  }
+
+  return true;
+
+}
