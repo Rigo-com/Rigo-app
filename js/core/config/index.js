cdcfe472f82340d341e11c10e59a693616f3dad0
Rigo-com <azizo436618@gmail.com>
2026-05-23 19:07:35 +0300

    Create index.js

---
+++ js/core/config/index.js
@@ -0,0 +1,29 @@
+// =====================================
+// RIGO AI
+// CORE CONFIG INDEX
+// =====================================
+
+
+
+const ConfigAPI =
+Object.freeze({
+
+  config:
+  APP_CONFIG,
+
+  runtime:
+  configRuntimeState,
+
+  get:
+  getConfigValue,
+
+  set:
+  setConfigValue,
+
+  has:
+  hasConfigValue,
+
+  reset:
+  resetRuntimeConfig
+
+});
