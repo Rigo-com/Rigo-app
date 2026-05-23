96b818e15ced19cc06ed08b2d784909635ad8199
Rigo-com <azizo436618@gmail.com>
2026-05-23 17:38:52 +0300

    Create index.js

---
+++ js/search/index.js
@@ -0,0 +1,18 @@
+// =====================================
+// RIGO AI
+// SEARCH INDEX
+// ENTERPRISE FINAL
+// =====================================
+
+
+
+const SearchAPI =
+Object.freeze({
+
+  search:
+  executeMemorySearch,
+
+  clearCache:
+  clearSearchCache
+
+});
