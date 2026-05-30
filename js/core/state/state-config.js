903618a5891885d7e258389043365ec573d185a6
Rigo-com <azizo436618@gmail.com>
2026-05-30 22:56:40 +0300

    Create state-config.js

---
+++ js/core/state/state-config.js
@@ -0,0 +1,48 @@
+// =====================================
+// RIGO AI
+// STATE CONFIG
+// =====================================
+
+
+
+// =====================================
+// CONFIG
+// =====================================
+
+const STATE_MANAGER_CONFIG =
+Object.freeze({
+
+  MAX_HISTORY:
+  500,
+
+  MAX_SNAPSHOTS:
+  100,
+
+  MAX_SUBSCRIBERS:
+  500,
+
+  ENABLE_HISTORY:
+  true,
+
+  ENABLE_SNAPSHOTS:
+  true,
+
+  ENABLE_MIDDLEWARE:
+  true,
+
+  ENABLE_TRANSACTIONS:
+  true,
+
+  ENABLE_ROLLBACKS:
+  true
+
+});
+
+
+
+// =====================================
+// EXPORTS
+// =====================================
+
+export default
+STATE_MANAGER_CONFIG;
