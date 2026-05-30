b0fe34e29c46b9ed53e37df83a5da712be53ab46
Rigo-com <azizo436618@gmail.com>
2026-05-30 21:08:01 +0300

    Update index.js

--- js/core/events/index.js
+++ js/core/events/index.js
@@ -8,3 +8,13 @@ export * from "./event-types.js";
 export * from "./event-manager.js";
 
 export * from "./event-app.js";
+
+export {
+  default as SystemEvents
+}
+from "./event-manager.js";
+
+export {
+  default as AppEvents
+}
+from "./event-app.js";
