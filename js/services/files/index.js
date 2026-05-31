7fa720d3c87863f0dce1beed56e43efcf23c2852
Rigo-com <azizo436618@gmail.com>
2026-05-31 19:43:29 +0300

    Create index.js

---
+++ js/services/files/index.js
@@ -0,0 +1,19 @@
+// =====================================
+// RIGO AI
+// FILES ENTRY POINT
+// =====================================
+
+export * from "./file-config.js";
+export * from "./file-events.js";
+export * from "./file-state.js";
+export * from "./file-utils.js";
+export * from "./file-validator.js";
+export * from "./file-factory.js";
+export * from "./file-manager.js";
+export * from "./file-reader.js";
+export * from "./file-url.js";
+export * from "./file-queue.js";
+export * from "./file-runtime.js";
+
+export { default }
+from "./file-runtime.js";
