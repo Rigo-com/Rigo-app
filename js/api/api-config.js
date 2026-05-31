1feccda60c55c0b7d1d15b1ceb07a5be5656de5c
Rigo-com <azizo436618@gmail.com>
2026-05-31 20:21:05 +0300

    Update api-config.js

--- js/api/api-config.js
+++ js/api/api-config.js
@@ -3,11 +3,8 @@
 // API CONFIG
 // =====================================
 
-const API_RUNTIME_CONFIG =
+const API_CONFIG =
 Object.freeze({
-
-  MAX_CONCURRENT_REQUESTS:
-  100,
 
   DEFAULT_TIMEOUT:
   30000,
@@ -15,85 +12,22 @@ Object.freeze({
   MAX_RETRIES:
   3,
 
-  RETRY_BASE_DELAY:
+  RETRY_DELAY:
   1000,
 
-  MAX_RETRY_DELAY:
-  10000,
-
-  MAX_QUEUE_RETRIES:
-  2,
+  MAX_CONCURRENT_REQUESTS:
+  100,
 
   UPLOAD_ENDPOINT:
-  "/files/upload",
-
-  ENABLE_DIAGNOSTICS:true,
-
-  ENABLE_EVENTS:true,
-
-  ENABLE_ABORT_CONTROLLERS:true,
-
-  ENABLE_REQUEST_TRACKING:true,
-
-  ENABLE_UPLOAD_TRACKING:true,
-
-  ENABLE_RESPONSE_NORMALIZATION:true,
-
-  ENABLE_PRIORITY_QUEUE:true
-
-});
-
-const VALID_API_STATUS =
-Object.freeze([
-
-  "idle",
-
-  "loading",
-
-  "success",
-
-  "error"
-
-]);
-
-const API_RUNTIME_EVENTS =
-Object.freeze({
-
-  REQUEST_STARTED:
-  "api.request.started",
-
-  REQUEST_SUCCESS:
-  "api.request.success",
-
-  REQUEST_FAILED:
-  "api.request.failed",
-
-  REQUEST_ABORTED:
-  "api.request.aborted",
-
-  REQUEST_QUEUED:
-  "api.request.queued",
-
-  REQUEST_DEQUEUED:
-  "api.request.dequeued",
-
-  UPLOAD_STARTED:
-  "api.upload.started",
-
-  UPLOAD_COMPLETED:
-  "api.upload.completed",
-
-  UPLOAD_FAILED:
-  "api.upload.failed"
+  "/files/upload"
 
 });
 
 export {
 
-  API_RUNTIME_CONFIG,
-
-  VALID_API_STATUS,
-
-  API_RUNTIME_EVENTS
+  API_CONFIG
 
 };
+
+export default
+API_CONFIG;
