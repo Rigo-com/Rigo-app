7881b7aecc478c00e39205f09bf25865181133fe
Rigo-com <azizo436618@gmail.com>
2026-05-31 19:34:28 +0300

    Create file-events.js

---
+++ js/services/files/file-events.js
@@ -0,0 +1,52 @@
+// =====================================
+// RIGO AI
+// FILE EVENTS
+// FILE EVENT DEFINITIONS
+// =====================================
+
+
+
+// =====================================
+// FILE EVENTS
+// =====================================
+
+const FILE_EVENTS =
+Object.freeze({
+
+  FILE_ADDED:
+  "file.added",
+
+  FILE_REMOVED:
+  "file.removed",
+
+  FILE_READ:
+  "file.read",
+
+  FILE_UPLOAD_STARTED:
+  "file.upload.started",
+
+  FILE_UPLOAD_COMPLETED:
+  "file.upload.completed",
+
+  FILE_UPLOAD_FAILED:
+  "file.upload.failed",
+
+  FILE_CLEARED:
+  "file.cleared"
+
+});
+
+
+
+// =====================================
+// EXPORTS
+// =====================================
+
+export {
+
+  FILE_EVENTS
+
+};
+
+export default
+FILE_EVENTS;
