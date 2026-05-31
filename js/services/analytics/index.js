ccbb682f432bace0fae672e3ce379da8f52206d3
Rigo-com <azizo436618@gmail.com>
2026-05-31 19:26:20 +0300

    Create index.js

---
+++ js/services/analytics/index.js
@@ -0,0 +1,9 @@
+// =====================================
+// RIGO AI
+// ANALYTICS ENTRY POINT
+// =====================================
+
+export * from "./analytics-runtime.js";
+
+export { default }
+from "./analytics-runtime.js";
