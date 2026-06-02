cd5bb7512172ab20cc97a78ceb0963a234ba2b75
Rigo-com <azizo436618@gmail.com>
2026-06-02 04:09:54 +0300

    Create bootstrap-config.js

---
+++ js/bootstrap/bootstrap-config.js
@@ -0,0 +1,18 @@
+// =====================================
+// RIGO AI
+// BOOTSTRAP CONFIG
+// =====================================
+
+export const BOOTSTRAP_CONFIG =
+Object.freeze({
+
+  ENABLE_DIAGNOSTICS:
+  true,
+
+  ENABLE_RECOVERY:
+  true,
+
+  MAX_RECOVERY_ATTEMPTS:
+  3
+
+});
