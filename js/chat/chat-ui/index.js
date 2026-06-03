70de551af8759d522416a3882748cfa69f783c2f
Rigo-com <azizo436618@gmail.com>
2026-06-03 22:01:19 +0300

    Create index.js

---
+++ js/chat/chat-ui/index.js
@@ -0,0 +1,34 @@
+// =====================================
+// RIGO AI
+// CHAT UI INDEX
+// =====================================
+
+export * from "./chat-elements.js";
+
+export * from "./chat-scroll-manager.js";
+
+export * from "./chat-ui-manager.js";
+
+export * from "./chat-ui-renderer.js";
+
+
+
+export {
+  default as ChatElements
+}
+from "./chat-elements.js";
+
+export {
+  default as ChatScrollManager
+}
+from "./chat-scroll-manager.js";
+
+export {
+  default as ChatUIManager
+}
+from "./chat-ui-manager.js";
+
+export {
+  default as ChatUIRenderer
+}
+from "./chat-ui-renderer.js";
