1190f4f207c32ede2aeb247e74f5938f4094473c
Rigo-com <azizo436618@gmail.com>
2026-06-03 22:00:21 +0300

    Create index.js

---
+++ js/chat/chat-actions/index.js
@@ -0,0 +1,11 @@
+// =====================================
+// RIGO AI
+// CHAT ACTIONS INDEX
+// =====================================
+
+export * from "./chat-actions.js";
+
+export {
+  default
+}
+from "./chat-actions.js";
