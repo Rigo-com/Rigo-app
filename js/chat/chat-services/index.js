8828e7b5e293df27eacbce38da1f62deb037a423
Rigo-com <azizo436618@gmail.com>
2026-06-03 21:54:30 +0300

    Create index.js

---
+++ js/chat/chat-services/index.js
@@ -0,0 +1,34 @@
+// =====================================
+// RIGO AI
+// CHAT SERVICES INDEX
+// =====================================
+
+export * from "./chat-message-service.js";
+
+export * from "./chat-queue-service.js";
+
+export * from "./chat-stream-service.js";
+
+export * from "./chat-render-service.js";
+
+
+
+export {
+  default as ChatMessageService
+}
+from "./chat-message-service.js";
+
+export {
+  default as ChatQueueService
+}
+from "./chat-queue-service.js";
+
+export {
+  default as ChatStreamService
+}
+from "./chat-stream-service.js";
+
+export {
+  default as ChatRenderService
+}
+from "./chat-render-service.js";
