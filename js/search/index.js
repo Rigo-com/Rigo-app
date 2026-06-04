0b94cc260f2ae9e8c8f9a932e5528e0cb3d9cfc2
Rigo-com <azizo436618@gmail.com>
2026-06-04 22:31:25 +0300

    Update index.js

--- js/search/index.js
+++ js/search/index.js
@@ -1,305 +1,286 @@
 // =====================================
 // RIGO AI
 // SEARCH INDEX
-// FINAL HARDENED EXPORT LAYER
+// PUBLIC ENTRY POINT
 // =====================================
 
+export {
 
+  SEARCH_LIMITS,
 
-// =====================================
-// IMPORTS
-// =====================================
+  SEARCH_TIMERS,
 
-import "./search-types.js";
-import "./search-state.js";
-import "./search-utils.js";
-import "./search-query.js";
-import "./search-cache.js";
-import "./search-ranking.js";
-import "./search-fuzzy.js";
-import "./search-snippets.js";
-import "./search-history.js";
-import "./search-worker.js";
+  SEARCH_FEATURES,
 
-import {
+  SEARCH_EVENTS,
 
-  rebuildSearchIndexes,
-  searchIndexedMemories
-
-} from "./search-index.js";
-
-import {
-
-  executeMemorySearch
-
-} from "./search-engine.js";
-
-
-
-// =====================================
-// VALIDATION
-// =====================================
-
-function validateSearchLayer(){
-
-  return (
-
-    typeof executeMemorySearch ===
-    "function"
-
-    &&
-
-    typeof rebuildSearchIndexes ===
-    "function"
-
-    &&
-
-    typeof searchIndexedMemories ===
-    "function"
-
-  );
+  SearchConfig
 
 }
+from "./search-config.js";
 
 
 
-// =====================================
-// SAFE ACCESS
-// =====================================
+export {
 
-function getSearchDiagnostics(){
+  searchState,
 
-  return Object.freeze({
+  setInitialized,
 
-    search:
+  setSearching,
 
-      typeof searchState !==
-      "undefined"
+  setHealthy,
 
-      ?
+  incrementActiveSearches,
 
-      searchState
+  decrementActiveSearches,
 
-      :
+  incrementSearches,
 
-      null,
+  incrementCompleted,
 
+  incrementFailed,
 
+  incrementAborted,
 
-    indexes:
+  incrementCacheHits,
 
-      typeof searchIndexState !==
-      "undefined"
+  incrementCacheMisses,
 
-      ?
+  getSearchSnapshot,
 
-      searchIndexState
-
-      :
-
-      null,
-
-
-
-    cache:
-
-      typeof searchCache !==
-      "undefined"
-
-      ?
-
-      searchCache
-
-      :
-
-      null,
-
-
-
-    history:
-
-      typeof searchHistoryState !==
-      "undefined"
-
-      ?
-
-      searchHistoryState
-
-      :
-
-      null,
-
-
-
-    worker:
-
-      typeof searchWorkerState !==
-      "undefined"
-
-      ?
-
-      searchWorkerState
-
-      :
-
-      null
-
-  });
-
-}
-
-
-
-// =====================================
-// SEARCH API
-// =====================================
-
-const RIGOSearch =
-Object.freeze({
-
-
-
-  // ===================================
-  // ENGINE
-  // ===================================
-
-  search:
-  executeMemorySearch,
-
-
-
-  // ===================================
-  // INDEX
-  // ===================================
-
-  rebuildIndexes:
-  rebuildSearchIndexes,
-
-
-
-  searchIndexed:
-  searchIndexedMemories,
-
-
-
-  // ===================================
-  // CACHE
-  // ===================================
-
-  clearCache:
-
-    typeof clearSearchCache ===
-    "function"
-
-    ?
-
-    clearSearchCache
-
-    :
-
-    null,
-
-
-
-  // ===================================
-  // HISTORY
-  // ===================================
-
-  popular:
-
-    typeof getPopularSearches ===
-    "function"
-
-    ?
-
-    getPopularSearches
-
-    :
-
-    null,
-
-
-
-  // ===================================
-  // WORKER
-  // ===================================
-
-  worker:
-
-    typeof executeSearchWorkerTask ===
-    "function"
-
-    ?
-
-    executeSearchWorkerTask
-
-    :
-
-    null,
-
-
-
-  // ===================================
-  // DIAGNOSTICS
-  // ===================================
-
-  diagnostics:
   getSearchDiagnostics,
 
+  resetSearchState,
+
+  SearchState
+
+}
+from "./search-state.js";
 
 
-  // ===================================
-  // VALIDATION
-  // ===================================
 
-  validate:
-  validateSearchLayer
+export {
+
+  initialize,
+
+  destroy,
+
+  startSearch,
+
+  completeSearch,
+
+  failSearch,
+
+  abortSearch,
+
+  health,
+
+  SearchCore
+
+}
+from "./search-core.js";
+
+
+
+export {
+
+  setCache,
+
+  getCache,
+
+  removeCache,
+
+  clearCache,
+
+  addHistory,
+
+  getHistory,
+
+  clearHistory,
+
+  cleanupExpiredCache,
+
+  cleanupExpiredHistory,
+
+  getStorageStats,
+
+  resetStorage,
+
+  SearchStorage
+
+}
+from "./search-storage.js";
+
+
+
+export {
+
+  on,
+
+  off,
+
+  once,
+
+  emit,
+
+  clear,
+
+  listenerCount,
+
+  SearchEvents
+
+}
+from "./search-events.js";
+
+
+
+export {
+
+  createSearchId,
+
+  normalizeQuery,
+
+  createCacheKey,
+
+  isValidQuery,
+
+  isValidSearchResult,
+
+  isValidSearchResults,
+
+  createSnippet,
+
+  createSearchResult,
+
+  normalizeError,
+
+  SearchHelpers
+
+}
+from "./search-helpers.js";
+
+
+
+export {
+
+  calculateScore,
+
+  sortResults,
+
+  filterResults,
+
+  deduplicateResults,
+
+  rankResults,
+
+  SearchRanking
+
+}
+from "./search-ranking.js";
+
+
+
+export {
+
+  executeIndexedSearch,
+
+  executeSemanticSearch,
+
+  executeFuzzySearch,
+
+  executeSearch,
+
+  SearchEngine
+
+}
+from "./search-engine.js";
+
+
+
+export {
+
+  getHealthStatus,
+
+  getDiagnostics,
+
+  getHealthReport,
+
+  isHealthy,
+
+  SearchHealth
+
+}
+from "./search-health.js";
+
+
+
+// =====================================
+// DEFAULT EXPORT
+// =====================================
+
+import SearchConfig
+from "./search-config.js";
+
+import SearchState
+from "./search-state.js";
+
+import SearchCore
+from "./search-core.js";
+
+import SearchStorage
+from "./search-storage.js";
+
+import SearchEvents
+from "./search-events.js";
+
+import SearchHelpers
+from "./search-helpers.js";
+
+import SearchRanking
+from "./search-ranking.js";
+
+import SearchEngine
+from "./search-engine.js";
+
+import SearchHealth
+from "./search-health.js";
+
+
+
+const Search =
+Object.freeze({
+
+  config:
+  SearchConfig,
+
+  state:
+  SearchState,
+
+  core:
+  SearchCore,
+
+  storage:
+  SearchStorage,
+
+  events:
+  SearchEvents,
+
+  helpers:
+  SearchHelpers,
+
+  ranking:
+  SearchRanking,
+
+  engine:
+  SearchEngine,
+
+  health:
+  SearchHealth
 
 });
 
 
 
-// =====================================
-// GLOBAL EXPORTS
-// =====================================
-
-if(
-  typeof globalThis ===
-  "object"
-){
-
-  Object.defineProperty(
-
-    globalThis,
-
-    "RIGOSearch",
-
-    {
-
-      value:
-      RIGOSearch,
-
-      writable:false,
-
-      configurable:false
-
-    }
-
-  );
-
-}
-
-
-
-// =====================================
-// EXPORTS
-// =====================================
-
-export {
-
-  RIGOSearch
-
-};
-
 export default
-RIGOSearch;
+Search;
