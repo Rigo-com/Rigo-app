db48c72a46673cf1adbcbfaf74b2ab2fe3c35fd4
Rigo-com <azizo436618@gmail.com>
2026-06-05 04:23:09 +0300

    Create index.js

---
+++ js/ui/i18n/index.js
@@ -0,0 +1,15 @@
+// =====================================
+// RIGO AI
+// UI I18N
+// PUBLIC EXPORTS
+// =====================================
+
+export {
+  LanguageRuntime
+}
+from "./language-runtime.js";
+
+export {
+  LanguageRuntime as default
+}
+from "./language-runtime.js";
