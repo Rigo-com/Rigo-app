41c485e255c691a896b77a2b2762d2db656a7a97
Rigo-com <azizo436618@gmail.com>
2026-06-06 23:09:26 +0300

    Update timestamps.js

--- js/debug/utils/timestamps.js
+++ js/debug/utils/timestamps.js
@@ -1,1 +1,33 @@
+// =====================================
+// RIGO AI
+// TIMESTAMPS
+// =====================================
 
+function now(){
+
+  return Date.now();
+
+}
+
+
+
+function iso(){
+
+  return new Date()
+  .toISOString();
+
+}
+
+
+
+export const Timestamps =
+Object.freeze({
+
+  now,
+
+  iso
+
+});
+
+export default
+Timestamps;
