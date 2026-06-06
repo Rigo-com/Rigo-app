86388be8c699fb65e3526c7d246ca4cd0d9d3cd3
Rigo-com <azizo436618@gmail.com>
2026-06-07 00:40:52 +0300

    Update module-scanner.js

--- js/debug-old/module-scanner.js
+++ js/debug-old/module-scanner.js
@@ -5,7 +5,15 @@
 
 const modules = [
 
-  "../core/runtime/index.js"
+  "../core/runtime/runtime-boot-sequence.js",
+
+  "../core/runtime/runtime-config.js",
+
+  "../core/runtime/runtime-helpers.js",
+
+  "../core/runtime/runtime-manager.js",
+
+  "../core/runtime/runtime-state.js"
 
 ];
 
