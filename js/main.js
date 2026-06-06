8d56525e38f8f5f1c0c9cfdcf8500ad73fcde018
Rigo-com <azizo436618@gmail.com>
2026-06-06 17:53:43 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,73 +3,14 @@
 // APPLICATION ENTRY POINT
 // =====================================
 
-console.log("MAIN START");
+import BootstrapManager
+from "./bootstrap/index.js";
 
 document.body.innerHTML = `
 <h1 style="color:green">
-MAIN WORKING
+BOOTSTRAP INDEX WORK
 </h1>
 `;
 
 
-
-// =====================================
-// STARTUP
-// =====================================
-
-async function startApplication(){
-
-  try{
-
-    console.log(
-      "RIGO BOOTING..."
-    );
-
-    const booted =
-    await BootstrapManager
-    .boot();
-
-    if(
-      !booted
-    ){
-
-      throw new Error(
-        "APPLICATION BOOT FAILED"
-      );
-
-    }
-
-    console.log(
-      "RIGO READY"
-    );
-
-  }
-
-  catch(error){
-
-  console.error(
-    "RIGO STARTUP ERROR",
-    error
-  );
-
-  document.body.innerHTML = `
-<pre style="
-color:red;
-padding:20px;
-white-space:pre-wrap;
-font-size:14px;
-">
-${error?.stack || error?.message || String(error)}
-</pre>
-`;
-
-}
-
-}
-
-
-
-// =====================================
-// BOOT
-// =====================================
 
