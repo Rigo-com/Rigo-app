ca5648ad5446968b8166639377ee699013f4f8f3
Rigo-com <azizo436618@gmail.com>
2026-06-06 18:37:11 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,14 +2,33 @@ try {
 
   await import("./bootstrap/index.js");
 
-  document.body.innerHTML = "OK";
+  document.body.innerHTML = `
+    <h1 style="color:green">
+      SUCCESS
+    </h1>
+  `;
 
 }
 catch(error){
 
   document.body.innerHTML = `
-<pre>
-${error.stack}
+<pre style="
+white-space:pre-wrap;
+padding:20px;
+color:red;
+font-size:14px;
+">
+TYPE:
+${typeof error}
+
+MESSAGE:
+${error?.message}
+
+STACK:
+${error?.stack}
+
+STRING:
+${String(error)}
 </pre>
 `;
 
