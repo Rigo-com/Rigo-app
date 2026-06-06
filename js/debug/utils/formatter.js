3cf028f458f0d8926e6bb8b09f80ebcc696e9662
Rigo-com <azizo436618@gmail.com>
2026-06-06 23:08:50 +0300

    Update formatter.js

--- js/debug/utils/formatter.js
+++ js/debug/utils/formatter.js
@@ -1,1 +1,50 @@
+// =====================================
+// RIGO AI
+// FORMATTER
+// =====================================
 
+function formatBytes(
+  bytes = 0
+){
+
+  if(bytes < 1024){
+    return `${bytes} B`;
+  }
+
+  if(bytes < 1048576){
+    return `${(bytes / 1024).toFixed(2)} KB`;
+  }
+
+  if(bytes < 1073741824){
+    return `${(bytes / 1048576).toFixed(2)} MB`;
+  }
+
+  return `${(bytes / 1073741824).toFixed(2)} GB`;
+
+}
+
+
+
+function formatDuration(
+  ms = 0
+){
+
+  return `${ms} ms`;
+
+}
+
+
+
+export const Formatter =
+Object.freeze({
+
+  bytes:
+  formatBytes,
+
+  duration:
+  formatDuration
+
+});
+
+export default
+Formatter;
