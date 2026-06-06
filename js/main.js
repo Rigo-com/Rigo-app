f2a89a9cb4ce633702c1fe49a311273ec7007d14
Rigo-com <azizo436618@gmail.com>
2026-06-07 02:18:03 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,2 @@
-import runModuleScanner
-from "./debug-old/module-scanner.js";
-
-runModuleScanner()
-.then(
-  results => {
-
-    document.body.innerHTML =
-
-    `<pre>${
-      JSON.stringify(
-        results,
-        null,
-        2
-      )
-    }</pre>`;
-
-  }
-);
+document.body.innerHTML =
+"<h1>MAIN WORKS</h1>";
