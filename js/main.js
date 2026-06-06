c4c95ce8bf997fbeabd7430996077b19d2932861
Rigo-com <azizo436618@gmail.com>
2026-06-06 07:39:52 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 import "./memory/memory-storage.js";
-import "./memory/memory-context.js";
-import "./memory/memory-embeddings.js";
 
 document.body.innerHTML = `
-<h1>GROUB 1 OK</h1>
+<h1>MEMORY STORAGE OK</h1>
 `;
