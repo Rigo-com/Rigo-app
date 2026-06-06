0a3b89c97463ca1220a8d9cdd097dd77553fe63a
Rigo-com <azizo436618@gmail.com>
2026-06-06 23:10:13 +0300

    Create index.js

---
+++ js/debug/utils/index.js
@@ -0,0 +1,71 @@
+// =====================================
+// RIGO AI
+// DEBUG UTILS INDEX
+// PUBLIC API
+// =====================================
+
+export {
+  DebugColors
+}
+from "./color-system.js";
+
+export {
+  Formatter
+}
+from "./formatter.js";
+
+export {
+  Logger
+}
+from "./logger.js";
+
+export {
+  Timestamps
+}
+from "./timestamps.js";
+
+
+
+import {
+  DebugColors
+}
+from "./color-system.js";
+
+import {
+  Formatter
+}
+from "./formatter.js";
+
+import {
+  Logger
+}
+from "./logger.js";
+
+import {
+  Timestamps
+}
+from "./timestamps.js";
+
+
+
+const DebugUtils =
+Object.freeze({
+
+  colors:
+  DebugColors,
+
+  formatter:
+  Formatter,
+
+  logger:
+  Logger,
+
+  timestamps:
+  Timestamps
+
+});
+
+
+
+export default
+DebugUtils;
