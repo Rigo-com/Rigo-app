813e97c868c5b9502376cda4906517329a5452e1
Rigo-com <azizo436618@gmail.com>
2026-06-08 02:31:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,21 +2,26 @@ import * as RIGO from "./index.js";
 
 const result = {};
 
-try{
+try {
+
+  await RIGO.UI.registerUiElements();
+
   result.root =
-    RIGO.ChatElements?.getRootElement?.() || null;
+    RIGO.ChatElements?.getRootElement?.();
 
   result.messages =
-    RIGO.ChatElements?.getMessagesElement?.() || null;
+    RIGO.ChatElements?.getMessagesElement?.();
 
   result.input =
-    RIGO.ChatElements?.getInputElement?.() || null;
+    RIGO.ChatElements?.getInputElement?.();
 
   result.sendButton =
-    RIGO.ChatElements?.getSendButtonElement?.() || null;
+    RIGO.ChatElements?.getSendButtonElement?.();
 
-}catch(error){
+} catch(error){
+
   result.error = error.message;
+
 }
 
 document.body.innerHTML =
