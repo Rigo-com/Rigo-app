de041c71ac14ebc1f540e52d77cb0caf1c1c8182
Rigo-com <azizo436618@gmail.com>
2026-06-08 02:50:40 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,44 @@
-import * as RIGO from "./index.js";
+(() => {
+
+const matches = [];
+
+for (const key in window) {
+
+  try {
+
+    const obj = window[key];
+
+    if (
+      typeof obj !== "object" &&
+      typeof obj !== "function"
+    ) continue;
+
+    const props =
+      Object.getOwnPropertyNames(obj);
+
+    props.forEach(prop => {
+
+      if (
+        prop.includes("Root") ||
+        prop.includes("root") ||
+        prop.includes("Element") ||
+        prop.includes("element")
+      ) {
+
+        matches.push({
+          object: key,
+          property: prop
+        });
+
+      }
+
+    });
+
+  } catch {}
+
+}
 
 document.body.innerHTML =
-`<pre>${
-RIGO.ChatElements
-?.registerElements
-?.toString?.()
-}</pre>`;
+`<pre>${JSON.stringify(matches,null,2)}</pre>`;
+
+})();
