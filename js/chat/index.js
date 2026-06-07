c3a2ab6f27cfb3277af682dd01e159f8c047ab9d
Rigo-com <azizo436618@gmail.com>
2026-06-07 22:08:52 +0300

    Update index.js

--- js/chat/index.js
+++ js/chat/index.js
@@ -59,3 +59,71 @@ export * from "./chat-actions/index.js";
 // =====================================
 
 export * from "./chat-ui/index.js";
+
+
+
+// =====================================
+// IMPORTS
+// =====================================
+
+import ChatStates
+from "./chat-state/index.js";
+
+import ChatEvents
+from "./chat-events/index.js";
+
+import ChatRuntime
+from "./chat-runtime/index.js";
+
+import ChatServices
+from "./chat-services/index.js";
+
+import ChatActions
+from "./chat-actions/index.js";
+
+import ChatUI
+from "./chat-ui/index.js";
+
+
+
+// =====================================
+// CHAT MODULE
+// =====================================
+
+const Chat =
+Object.freeze({
+
+  State:
+  ChatStates,
+
+  Events:
+  ChatEvents,
+
+  Runtime:
+  ChatRuntime,
+
+  Services:
+  ChatServices,
+
+  Actions:
+  ChatActions,
+
+  UI:
+  ChatUI
+
+});
+
+
+
+// =====================================
+// EXPORTS
+// =====================================
+
+export {
+
+  Chat
+
+};
+
+export default
+Chat;
