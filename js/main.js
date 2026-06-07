b7212d97628f1bc263d6d892dd6276dca6a2a96b
Rigo-com <azizo436618@gmail.com>
2026-06-08 00:00:15 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,10 +3,54 @@ import {
 }
 from "./index.js";
 
-document.body.innerHTML =
-  "<h1>BEFORE BOOT</h1>";
 
-await Bootstrap.boot();
 
-document.body.innerHTML =
-  "<h1>AFTER BOOT</h1>";
+async function startApplication(){
+
+  document.body.innerHTML =
+    "<h1>BOOT STARTED</h1>";
+
+  try{
+
+    const systems =
+      Bootstrap.list();
+
+    document.body.innerHTML =
+      `<pre>
+REGISTERED SYSTEMS:
+
+${
+  JSON.stringify(
+    systems,
+    null,
+    2
+  )
+}
+      </pre>`;
+
+    await Bootstrap.boot();
+
+    document.body.innerHTML +=
+      "<hr><h1>BOOT SUCCESS</h1>";
+
+  }
+  catch(error){
+
+    document.body.innerHTML =
+      `<pre>
+BOOT FAILED
+
+${
+  error?.stack ||
+  error?.message ||
+  String(error)
+}
+      </pre>`;
+
+  }
+
+}
+
+
+
+startApplication();
