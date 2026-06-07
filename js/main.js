d54888ec2c28b906661a0fc77f3c0702f55f302e
Rigo-com <azizo436618@gmail.com>
2026-06-08 01:55:28 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,58 +1,11 @@
 import * as RIGO from "./index.js";
 
-document.body.innerHTML = "";
+console.log("UI", RIGO.UI);
+console.log("UiRuntime", RIGO.UiRuntime);
 
-const suspects = [];
+await RIGO.UiRuntime?.initializeUi?.();
 
-for(
-  const [moduleName,module]
-  of Object.entries(RIGO)
-){
+await RIGO.UiRuntime?.render?.();
 
-  if(
-    typeof module !==
-    "object"
-  ){
-    continue;
-  }
-
-  for(
-    const key
-    of Object.keys(module)
-  ){
-
-    if(
-
-         key.includes("render")
-      || key.includes("Render")
-      || key.includes("create")
-      || key.includes("Create")
-      || key.includes("bootstrap")
-      || key.includes("initialize")
-
-    ){
-
-      suspects.push({
-
-        module:
-        moduleName,
-
-        function:
-        key
-
-      });
-
-    }
-
-  }
-
-}
-
-document.body.innerHTML =
-"<pre>" +
-JSON.stringify(
-  suspects,
-  null,
-  2
-) +
-"</pre>";
+document.body.innerHTML +=
+"<h1>UI CALLED</h1>";
