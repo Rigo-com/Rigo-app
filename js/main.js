ce0c1bfcfec09227f6f89b404c9b3a31adc91fa6
Rigo-com <azizo436618@gmail.com>
2026-06-08 01:45:39 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,117 +1,27 @@
-import * as RIGO
-from "./index.js";
+import * as RIGO from "./index.js";
 
 document.body.innerHTML = "";
 
-function print(
-  title,
-  value
-){
+const targets = [
+  "app",
+  "appRoot",
+  "chatContainer",
+  "messagesContainer",
+  "messageInput",
+  "sendButton"
+];
 
-  const pre =
-  document.createElement(
-    "pre"
-  );
+const pre =
+document.createElement("pre");
 
-  pre.textContent =
-  title +
-  "\n\n" +
-  JSON.stringify(
-    value,
-    null,
-    2
-  );
+pre.textContent =
+JSON.stringify(
+{
+  exports:Object.keys(RIGO),
+  targets
+},
+null,
+2
+);
 
-  document.body
-  .appendChild(
-    pre
-  );
-
-}
-
-for(
-  const [name,module]
-  of Object.entries(
-    RIGO
-  )
-){
-
-  if(
-    !module ||
-    typeof module !==
-    "object"
-  ){
-
-    continue;
-
-  }
-
-  const keys =
-  Object.keys(
-    module
-  );
-
-  const interesting =
-
-    keys.filter(
-      key =>
-
-        key
-        .toLowerCase()
-        .includes(
-          "init"
-        )
-
-        ||
-
-        key
-        .toLowerCase()
-        .includes(
-          "boot"
-        )
-
-        ||
-
-        key
-        .toLowerCase()
-        .includes(
-          "start"
-        )
-
-        ||
-
-        key
-        .toLowerCase()
-        .includes(
-          "render"
-        )
-
-        ||
-
-        key
-        .toLowerCase()
-        .includes(
-          "mount"
-        )
-
-        ||
-
-        key
-        .toLowerCase()
-        .includes(
-          "create"
-        )
-    );
-
-  if(
-    interesting.length
-  ){
-
-    print(
-      name,
-      interesting
-    );
-
-  }
-
-}
+document.body.appendChild(pre);
