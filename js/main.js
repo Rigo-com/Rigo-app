e1441810359d442633bf18c807e83eb1b17a77a1
Rigo-com <azizo436618@gmail.com>
2026-06-08 05:52:17 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,41 @@
-alert("RIGO MAIN STARTED");
+alert("MAIN OK");
+
+import {
+  Bootstrap
+}
+from "./index.js";
+
+alert("INDEX OK");
+
+async function startApplication(){
+
+  try{
+
+    alert("BOOT START");
+
+    await Bootstrap.boot();
+
+    alert("BOOT SUCCESS");
+
+  }
+  catch(error){
+
+    alert(
+      "BOOT FAILED"
+    );
+
+    console.error(
+      error
+    );
+
+    document.body.innerHTML =
+    `<pre>${error?.stack || error}</pre>`;
+
+  }
+
+}
+
+startApplication();
 
 // =====================================
 // RIGO AI
