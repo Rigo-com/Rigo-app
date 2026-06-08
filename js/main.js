f5b4b503ea252e1a984f7e636486323cb5e40501
Rigo-com <azizo436618@gmail.com>
2026-06-08 03:29:12 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,8 @@
-(() => {
+import createWelcomeScreen from "./welcome-screen.js";
 
-const checks = {
-  createElement: false,
-  setRootElement: false,
-  registerUiElements: false,
-  bootstrapUi: false,
-  initializeUi: false
-};
-
-for (const key in window) {
-
-  try {
-
-    const obj = window[key];
-
-    if (
-      typeof obj !== "function" &&
-      typeof obj !== "object"
-    ) continue;
-
-    const text = String(obj);
-
-    Object.keys(checks)
-      .forEach(name => {
-
-        if (
-          text.includes(name)
-        ) {
-          checks[name] = true;
-        }
-
-      });
-
-  } catch {}
-
-}
-
-document.body.innerHTML =
-`<pre>${JSON.stringify(checks,null,2)}</pre>`;
-
-})();
+window.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    createWelcomeScreen();
+  }
+);
