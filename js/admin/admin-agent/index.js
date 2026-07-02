6375ab91344cd056e4860e1c28a38fdc261f7959
Rigo-com <azizo436618@gmail.com>
2026-07-02 21:24:39 +0300

    Update index.js

--- js/admin/admin-agent/index.js
+++ js/admin/admin-agent/index.js
@@ -1,1 +1,17 @@
+// =====================================
+// RIGO AI
+// ADMIN AGENT
+// ROOT EXPORTS
+// =====================================
 
+export * from "./admin-agent.js";
+export { default as AdminAgent }
+from "./admin-agent.js";
+
+export * from "./admin-agent-state.js";
+export { default as AdminAgentState }
+from "./admin-agent-state.js";
+
+export * from "./admin-agent-permissions.js";
+export { default as AdminAgentPermissions }
+from "./admin-agent-permissions.js";
