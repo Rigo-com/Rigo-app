69009ff178b443692654cabca0c6c67c8271a824
Rigo-com <azizo436618@gmail.com>
2026-07-02 22:15:42 +0300

    Update index.js

--- js/admin/subagents/project-agent/index.js
+++ js/admin/subagents/project-agent/index.js
@@ -4,6 +4,10 @@
 // PRIVATE SUBAGENT EXPORTS
 // =====================================
 
+export * from "./project-agent.js";
+export { default as ProjectAgent }
+from "./project-agent.js";
+
 export * from "./project-agent-state.js";
 export { default as ProjectAgentState }
 from "./project-agent-state.js";
