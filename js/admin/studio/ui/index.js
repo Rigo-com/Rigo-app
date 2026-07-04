28431736092dbfdbfe7787fae646e50fec7de77c
Rigo-com <azizo436618@gmail.com>
2026-07-04 21:37:46 +0300

    Update index.js

--- js/admin/studio/ui/index.js
+++ js/admin/studio/ui/index.js
@@ -1,1 +1,25 @@
+// =====================================
+// RIGO AI
+// STUDIO UI
+// ROOT EXPORTS
+// =====================================
 
+import StudioUI
+from "./studio-ui.js";
+
+export * from "./studio-ui.js";
+export { default as StudioUI }
+from "./studio-ui.js";
+
+export * from "./studio-layout.js";
+
+export * from "./studio-sidebar.js";
+export { default as renderStudioSidebar }
+from "./studio-sidebar.js";
+
+export * from "./studio-theme.js";
+export { default as StudioTheme }
+from "./studio-theme.js";
+
+export default
+StudioUI;
