a7b7d08a93d46c86e7a59e799a328a2d01eb21a3
Rigo-com <azizo436618@gmail.com>
2026-08-09 20:27:20 +0300

    Remove temporary environment diagnostic

--- api/env-check.js
+++
@@ -1,7 +0,0 @@
-export default function handler(request,response){
-  response.setHeader("Cache-Control","no-store");
-  const keys=Object.keys(process.env)
-    .filter(key=>/(NEON|DATABASE|AUTH)/i.test(key))
-    .sort();
-  response.status(200).json({ok:true,keys});
-}
