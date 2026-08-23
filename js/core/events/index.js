1a4b897d1b2f537a747fbecb89bd73f58cef2209
Rigo-com <azizo436618@gmail.com>
2026-08-23 21:59:15 +0300

    core(events): expose lifecycle and snapshot contract

--- js/core/events/index.js
+++ js/core/events/index.js
@@ -10,15 +10,47 @@ import AppEvents
 from "./event-app.js";
 
 export * from "./event-types.js";
-
 export * from "./event-manager.js";
-
 export * from "./event-app.js";
+
+function initialize(){
+  return SystemEvents.initialize();
+}
+
+const boot = initialize;
+
+function shutdown(){
+  return true;
+}
+
+function reset(){
+  return SystemEvents.reset();
+}
+
+function snapshot(){
+  return Object.freeze({
+    system:SystemEvents.diagnostics(),
+    timestamp:Date.now()
+  });
+}
+
+const Events =
+Object.freeze({
+  ...SystemEvents,
+  id:"core-events",
+  priority:15,
+  app:AppEvents,
+  initialize,
+  boot,
+  shutdown,
+  reset,
+  snapshot
+});
 
 export {
   SystemEvents,
-  AppEvents
+  AppEvents,
+  Events
 };
 
-export default
-SystemEvents;
+export default Events;
