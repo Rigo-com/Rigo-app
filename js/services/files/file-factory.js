fb4ac3445f66deb30fdf7ce287bc85df20a9bba2
Rigo-com <azizo436618@gmail.com>
2026-09-23 00:58:47 +0300

    fix(services): normalize file entities

--- js/services/files/file-factory.js
+++ js/services/files/file-factory.js
@@ -1,99 +1,13 @@
-// =====================================
-// RIGO AI
-// FILE FACTORY
-// FILE ENTITY FACTORY
-// =====================================
-
-import {
-
-  validateFile
-
-}
-from "./file-validator.js";
-
-import {
-
-  createFileId,
-
-  sanitizeFileName,
-
-  getFileExtension,
-
-  createFileFingerprint
-
-}
-from "./file-utils.js";
-
-
-
-// =====================================
-// FILE OBJECT
-// =====================================
-
-function createFileObject(
-  file
-){
-
-  if(
-    !validateFile(
-      file
-    )
-  ){
-
-    return null;
-
-  }
-
-  const sanitizedName =
-  sanitizeFileName(
-    file.name
-  );
-
+import { validateFile } from "./file-validator.js";
+import { createFileId,sanitizeFileName,getFileExtension,createFileFingerprint } from "./file-utils.js";
+function createFileObject(file){
+  if(!validateFile(file)) return null;
+  const sanitizedName=sanitizeFileName(file.name);
+  if(!sanitizedName) return null;
   return Object.freeze({
-
-    id:
-    createFileId(),
-
-    name:
-    sanitizedName,
-
-    size:
-    file.size,
-
-    type:
-    file.type,
-
-    extension:
-    getFileExtension(
-      sanitizedName
-    ),
-
-    lastModified:
-    file.lastModified,
-
-    createdAt:
-    Date.now(),
-
-    fingerprint:
-    createFileFingerprint(
-      file
-    ),
-
-    rawFile:
-    file
-
+    id:createFileId(),name:sanitizedName,size:file.size,type:file.type,
+    extension:getFileExtension(sanitizedName),lastModified:Number(file.lastModified)||0,
+    createdAt:Date.now(),fingerprint:createFileFingerprint(file),rawFile:file
   });
-
 }
-
-
-
-// =====================================
-// EXPORTS
-// =====================================
-
-export {
-
-  createFileObject
-
-};
+export {createFileObject};
