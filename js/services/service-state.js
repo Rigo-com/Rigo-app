b895c80e30b1de8497bb8fc4117e68ae1dea4c9a
Rigo-com <azizo436618@gmail.com>
2026-09-23 01:02:53 +0300

    cleanup(services): remove unused synchronization state

--- js/services/service-state.js
+++ js/services/service-state.js
@@ -1,6 +1,5 @@
 const serviceState = Object.seal({
   initialized:false,
-  synchronized:false,
   booted:false,
   startedAt:null,
   stoppedAt:null,
@@ -14,7 +13,6 @@ const serviceState = Object.seal({
 function createServiceStateSnapshot(){
   return Object.freeze({
     initialized:serviceState.initialized,
-    synchronized:serviceState.synchronized,
     booted:serviceState.booted,
     startedAt:serviceState.startedAt,
     stoppedAt:serviceState.stoppedAt,
@@ -25,7 +23,6 @@ function createServiceStateSnapshot(){
 
 function resetServiceState(){
   serviceState.initialized = false;
-  serviceState.synchronized = false;
   serviceState.booted = false;
   serviceState.startedAt = null;
   serviceState.stoppedAt = null;
