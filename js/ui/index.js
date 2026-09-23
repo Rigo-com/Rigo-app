e485cffad11ff33d02e99e29205d313afb4b1811
Rigo-com <azizo436618@gmail.com>
2026-09-23 17:51:58 +0300

    refactor(ui): remove unused i18n state and configuration

--- js/ui/index.js
+++ js/ui/index.js
@@ -5,18 +5,13 @@
 // =====================================
 
 export {
-  UI_CONFIG,
-  uiState,
-  UiState
+  UiState,
+  uiState
 } from "./ui-state.js";
 
 export {
   UiRenderer
 } from "./ui-renderer.js";
-
-export {
-  LanguageRuntime
-} from "./i18n/index.js";
 
 export {
   UiRuntime
